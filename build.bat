8afa13fe041489183402085921b790e25fd6fe81
Silverdragon122 <101513517+Silverdragon122@users.noreply.github.com>
2025-12-13 22:28:25 -0500

    Delete build.bat
    Oops didnt mean to add this :)

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist "Release" mkdir "Release"
-"src\DistributionTool.exe" -b -i "src\sh.cider.streamdeck.sdPlugin" -o "Release"
-echo Build complete. Check the Release folder.
-pause
